1a8dceab1c1c93538b815e7b1b6412a762d8e402
Hanna Lysenko <hannatkach92@gmail.com>
2020-01-08 19:25:20 -0600

    Home work #1

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 function sum (a, b){
     return a + b;
-}
+};
 
-function mult(a, b){
+function mult (a, b){
     return a * b;
-}
+};
 
-function isAdult(age){
-    if(age >= 18) return true;
-    else return false;
-}
+function isAdult (age){
+    return age >= 18;
+};
 
 function isPalindrome(str){
-    let srt1 = '';
-    for (let i = 0; i < str.length; i++){
-        str1 += str[i];
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
     }
-    return (str === str1);
-}
+    return (str === newStr);
+};
 
-function isOddNumber(num){
+function isOddNumber (num){
     return (num % 2 !== 0);
-}
+};
 
-function isEvenNumber(num){
+function isEvenNumber (num){
     return (num % 2 === 0);
-}
+};
 
-function sortArrayUp(arr){
+function sortArrayUp (arr){
     return arr.sort();
-}
+};
 
-module.exports = {sum,   mult, isAdult, isPalindrome, isOddNumber, isEvenNumber, sortArrayUp};
+module.exports = {sum, mult, isAdult, isPalindrome, isOddNumber, isEvenNumber, sortArrayUp};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,68 @@
 const assert = require('assert')
-const {sum, mult} = require('../index.js')
+const {sum, mult, isAdult, isPalindrome, isOddNumber, isEvenNumber, sortArrayUp} = require('../index.js')
 
-it ('should sum equals 10', () => {
-    assert.equal(sum(4, 6), 10);
+describe('function sum', () => {
+    it('should sum equals 10', () => {
+        assert.equal(sum(4, 6), 10);
+    });
+
+    it('should sum not equals 8', () => {
+        assert.notEqual(sum(4, 6), 8)
+    });
 });
 
-it ('should sum not equals 8', () => {
-    assert.notEqual(sum(4, 6), 5)
+describe ('function mult', () => {
+    it('shoult mult equals 32', () => {
+        assert.equal(mult(4, 8), 32)
+    });
+
+    it('shoult mult not equal 12', () => {
+        assert.notEqual(mult(3, 6), 12)
+    });
 });
 
-it ('shoult mult equals 32npm test', () => {
-    assert.equal(mult(4, 8), 32)
-})
+describe('function isAdult', () => {
+    it('should function IsAdult is true', () => {
+        assert.equal(isAdult(18), true)
+    });
+
+    it('should function IsAdult is false', () => {
+        assert.equal(isAdult(15), false)
+    });
+});
+
+describe ('function isPalindrome', () => {
+    it ('should function isPalindrome is palindrome', () => {
+        assert.equal(isPalindrome('ogo'), true);
+    });
+
+    it ('should function isPalindrome is not palindrome', () => {
+        assert.equal(isPalindrome('mama'), false);
+    });
+});
+
+describe ('function isOddNumber', () => {
+    it ('function isOddNumber should contain an odd number', () => {
+        assert.equal(isOddNumber(7), true);
+    });
+
+    it ('function isOddNumber should not contain an odd number', () => {
+        assert.equal(isOddNumber(8), false);
+    });
+});
+
+describe ('function isOddNumber', () => {
+    it ('function isEvenNumber should contain an even number', () => {
+        assert.equal(isEvenNumber(8), true);
+    });
+
+    it (' function isEvenNumber should not contain an even number', () => {
+        assert.equal(isEvenNumber(7), false);
+    });
+});
+
+describe ('function sortArrayUp', () => {
+    it ('function sortArrayUp should sort numbers', () => {
+        assert.deepEqual(sortArrayUp([5, 8, 7, 6]), [5, 6, 7, 8]);
+    });
+});
