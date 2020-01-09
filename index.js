45bf308f574f96af2041ea3ef56a4e8c003b4088
Hanna Lysenko <hannatkach92@gmail.com>
2020-01-08 18:25:33 -0600

    Home work #1

--- index.js
+++ index.js
@@ -5,4 +5,30 @@ function sum (a, b){
 function mult(a, b){
     return a * b;
 }
-module.exports = {sum,   mult};
+
+function isAdult(age){
+    if(age >= 18) return true;
+    else return false;
+}
+
+function isPalindrome(str){
+    let srt1 = '';
+    for (let i = 0; i < str.length; i++){
+        str1 += str[i];
+    }
+    return (str === str1);
+}
+
+function isOddNumber(num){
+    return (num % 2 !== 0);
+}
+
+function isEvenNumber(num){
+    return (num % 2 === 0);
+}
+
+function sortArrayUp(arr){
+    return arr.sort();
+}
+
+module.exports = {sum,   mult, isAdult, isPalindrome, isOddNumber, isEvenNumber, sortArrayUp};
